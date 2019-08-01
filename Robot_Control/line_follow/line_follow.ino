81468650678137e478505ed060a5fbae6cd3f4c6
shane <shane@localhost.localdomain>
2019-08-01 17:55:39 +0100

    modified to work with robo lib

--- Robot_Control/line_follow/line_follow.ino
+++ Robot_Control/line_follow/line_follow.ino
@@ -2,19 +2,16 @@
 // RoboSlam example: Colour sensor control
 //
 
-#define LEFT_MOTOR_A  2
-#define LEFT_MOTOR_B  3
-#define RIGHT_MOTOR_A 4
-#define RIGHT_MOTOR_B 5
+#include <robojam.h>
 
 // Declare an integer variable to store the colour readings
-int colour;
+int ir_val;
 
 // The setup routine runs once when the power is switched on.
 void setup()
 {
   // Digital output for LED
-  pinMode(2, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
 
   // Digital outputs for left motor
   pinMode(LEFT_MOTOR_A, OUTPUT);
@@ -27,51 +24,34 @@ void setup()
   // Open serial port to display colour readings on screen
   Serial.begin(9600);
 
-  analogWrite(10, 140);
+  set_speed(1);
 }
 
 // The loop routine runs over and over until the power is switched off.
 void loop()
 {
   // Read the voltage on pin A0 (the colour sensor voltage)
-  colour = analogRead(A0);
+  ir_val = analogRead(IR_PIN);
 
   // Print the colour value via the serial port
-  Serial.println(colour);
+  Serial.println(ir_val);
   
   // Choose motor action based on colour reading
-  if (colour > 100)
+  if (ir_val > 100)
   {
     // black detected
     digitalWrite(2, LOW);  // LED off
-    turn_right();
+    shuffle_left();
   }
   else
   {
     // white detected
     digitalWrite(2, HIGH); // LED on
-    turn_left();
+    shuffle_right();
   }
 }
 
 
  
-void turn_left(){
-  
-  digitalWrite(LEFT_MOTOR_A, LOW); // Left motor reverse
-  digitalWrite(LEFT_MOTOR_B, LOW);
-  digitalWrite(RIGHT_MOTOR_A, HIGH);  // Right motor forward
-  digitalWrite(RIGHT_MOTOR_B, LOW);
-  
-}
- 
-void turn_right(){
-  
-  digitalWrite(LEFT_MOTOR_A, HIGH); // Left motor forward
-  digitalWrite(LEFT_MOTOR_B, LOW);
-  digitalWrite(RIGHT_MOTOR_A, LOW);  // Right motor reverse
-  digitalWrite(RIGHT_MOTOR_B, LOW);
-  
-}
- 
+
 
