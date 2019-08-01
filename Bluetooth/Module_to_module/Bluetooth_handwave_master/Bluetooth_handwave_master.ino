9f72cbc25911552732b6299df02009b694e8acdf
shane <shane@localhost.localdomain>
2019-08-01 22:54:17 +0100

    modified to work with robojam lib

--- Bluetooth/Module_to_module/Bluetooth_handwave_master/Bluetooth_handwave_master.ino
+++ Bluetooth/Module_to_module/Bluetooth_handwave_master/Bluetooth_handwave_master.ino
@@ -6,27 +6,23 @@
  */
 
  #include <SoftwareSerial.h>
+ #include <robojam.h>
 
-SoftwareSerial btSerial(A4,A5); // RX, TX
- 
-int trigPin = 6;    // Trigger
-int echoPin = 7;    // Echo
-int ledPin = LED_BUILTIN;
-long duration, cm, distance;
+SoftwareSerial btSerial(BLUETOOTH_RX, BLUETOOTH_TX); // RX, TX
+
+long distance;
  
 void setup() {
-  //Serial Port begin
+  
+  //Serial Ports begin
   Serial.begin (9600);
   btSerial.begin(9600);
-
   
   //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
 
-  pinMode(ledPin, OUTPUT);
-
-  
 }
  
 void loop() {
@@ -35,7 +31,7 @@ void loop() {
   
   if (distance < 30)         //hand detected
   {
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     btSerial.print("a");
     while (get_distance() < 30) {}  //wait until hand is removed
     btSerial.print("d");
@@ -43,38 +39,11 @@ void loop() {
   }
   else
   {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(LED_PIN, LOW);
   }
 
   
 }
 
-int get_distance() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  
-  if (cm < 100)
-  {
-  return cm;
-  }
-  else
-  {
-    return 100;
-  }
-  
-}
+
 
