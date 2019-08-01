e9856946df91b50a07cc72d57097b7f4bffc0294
shane <shane@localhost.localdomain>
2019-08-01 17:26:57 +0100

    modified to work with robojam lib

--- Sensor_Examples/ultrasonic_led_control/ultrasonic_led_control.ino
+++ Sensor_Examples/ultrasonic_led_control/ultrasonic_led_control.ino
@@ -4,55 +4,34 @@
  * prints the measured distance in centimeters over the serial port
  */
  
-int trigPin = 6;    // Trigger
-int echoPin = 7;    // Echo
-int ledPin = 11;
-long duration, cm, distance;
+#include <robojam.h>
+
+long distance;
  
 void setup() {
+  
   //Serial Port begin
   Serial.begin (9600);
+  
   //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  pinMode(ledPin, OUTPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  
 }
+
  
 void loop() {
-	
+
 	distance = get_distance();
+  
 	if (distance != 100)
   {
-	  analogWrite(ledPin, distance*2.5);
+	  analogWrite(LED_PIN, distance*2.5);
   }
+
+  Serial.println(distance);
+  delay(50);
 }
 
-int get_distance() {
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
-  
-  if (cm < 100)
-  {
-	return cm;
-  }
-  else
-  {
-	  return 100;
-  }
-  
-}
+
--- Sensor_Examples/ultrasonic_print_distance/ultrasonic_print_distance.ino
+++ Sensor_Examples/ultrasonic_print_distance/ultrasonic_print_distance.ino
@@ -4,32 +4,33 @@
  * prints the measured distance in centimeters over the serial port
  */
  
-int trigPin = 6;    // Trigger
-int echoPin = 7;    // Echo
-long duration, cm ;
+ #include <robojam.h>
  
 void setup() {
   //Serial Port begin
   Serial.begin (9600);
   //Define inputs and outputs
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
  
 void loop() {
+
+  static long duration, cm;
+  
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(5);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
  
   // Read the signal from the sensor: a HIGH pulse whose
   // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
+  
+  duration = pulseIn(ECHO_PIN, HIGH);
  
   // Convert the time into a distance
   cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
