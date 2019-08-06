f532ae4984639b9b93c998a8667b5d6cd1cbed4f
shane <shane@localhost.localdomain>
2019-08-06 16:11:20 +0100

    modified example to change reg green blink rate

--- Sensor_Examples/ultrasonic_led_control/ultrasonic_led_control.ino
+++ Sensor_Examples/ultrasonic_led_control/ultrasonic_led_control.ino
@@ -7,6 +7,9 @@
 #include <robojam.h>
 
 long distance;
+unsigned long previousMillis;
+int interval;
+bool ledState = 0;
  
 void setup() {
   
@@ -17,6 +20,7 @@ void setup() {
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
   pinMode(GREEN_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
   
 }
 
@@ -24,14 +28,27 @@ void setup() {
 void loop() {
 
 	distance = get_distance();
+  interval = (distance*5);
   
-	if (distance != 100)
-  {
-	  analogWrite(GREEN_LED, distance*2.5);
-  }
+	unsigned long currentMillis = millis();
 
-  Serial.println(distance);
-  delay(50);
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = 1;
+      digitalWrite(GREEN_LED, HIGH);
+      digitalWrite(RED_LED  , LOW);
+    } else {
+      ledState = 0;
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(RED_LED  , HIGH);
+    }
+
+   
+  }
 }
 
 
