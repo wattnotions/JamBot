d7530deffa92464383440de1658367568d0b8d9c
shane <shane@localhost.localdomain>
2019-08-06 16:12:16 +0100

    used robojam lib to make example shorter

--- Sensor_Examples/ultrasonic_print_distance/ultrasonic_print_distance.ino
+++ Sensor_Examples/ultrasonic_print_distance/ultrasonic_print_distance.ino
@@ -7,8 +7,10 @@
  #include <robojam.h>
  
 void setup() {
+  
   //Serial Port begin
   Serial.begin (9600);
+  
   //Define inputs and outputs
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
@@ -16,32 +18,14 @@ void setup() {
  
 void loop() {
 
-  static long duration, cm;
-  
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(5);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  
-  duration = pulseIn(ECHO_PIN, HIGH);
- 
-  // Convert the time into a distance
-  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  static long distance;
 
-
-  if(cm<100){
-    Serial.print(cm);
-    Serial.println();
+  distance = get_distance();
+  
+  if (distance<100){
+    Serial.println(distance);
   }
-  
-  
-  delay(20);
+
+  delay(50);
 }
 
